342871c3bfcb4f80f5a9da97de39f66001cf8905
Alec <alec.burmeister@gmail.com>
2026-05-09 10:57:39 -0500

    Few more paneling

--- Scripts/RuneShopButton.gd
+++ Scripts/RuneShopButton.gd
@@ -9,12 +9,12 @@ func setup(r: RuneData, main_node:MainNode, main_menu_node:MainMenu, shop:ShopPa
 	main = main_node
 	main_menu = main_menu_node
 	shop_panel = shop
-	$Button/RuneTexture.texture = r.icon
-	$Button/RuneTexture/Label.text = str(r.buy_cost)
-	$Button/RuneTexture/owned.text = str("Owned: ", main.game_data.get_rune_count(r.name))
-	$Button.pressed.connect(buy_rune.bind(rune))
+	$Panel/RuneTexture.texture = r.icon
+	$Panel/RuneTexture/Label.text = str(r.buy_cost)
+	$Panel/RuneTexture/owned.text = str("Owned: ", main.game_data.get_rune_count(r.name))
+	$Panel/Button.pressed.connect(buy_rune.bind(rune))
 
 func buy_rune(r:RuneData) -> void:
 	shop_panel.buy_rune(r)
 	# Next update the owned label
-	$Button/RuneTexture/owned.text = str("Owned: ", main.game_data.get_rune_count(r.name))
+	$Panel/RuneTexture/owned.text = str("Owned: ", main.game_data.get_rune_count(r.name))
