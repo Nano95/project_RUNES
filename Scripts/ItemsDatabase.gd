3208a89e369d78a0e00477972cb1c3cc0dc77fdc
Alec <alec.burmeister@gmail.com>
2026-06-26 14:04:52 -0500

    finish placeholder sprites for loot

--- Scripts/ItemsDatabase.gd
+++ Scripts/ItemsDatabase.gd
@@ -46,4 +46,49 @@ var loot_data = {
 		"short_name": "",
 		"icon": preload("res://Sprites/Loot/elemental_orb.png"),
 	},
+	"Bat Wing": {
+		"name": "Bat Wing",
+		"short_name": "",
+		"icon": preload("res://Sprites/Loot/bat_wing.png"),
+	},
+	"Beast Fang": {
+		"name": "Beast Fang",
+		"short_name": "",
+		"icon": preload("res://Sprites/Loot/beast_fang.png"),
+	},
+	#"Iron Ore": {
+		#"name": "Iron Ore",
+		#"short_name": "",
+		#"icon": preload(),
+	#},
+	"Monster Eye": {
+		"name": "Monster Eye",
+		"short_name": "",
+		"icon": preload("res://Sprites/Loot/monster_eye.png"),
+	},
+	"Refined Iron": {
+		"name": "Refined Iron",
+		"short_name": "",
+		"icon": preload("res://Sprites/Loot/refined_iron.png"),
+	},
+	"Spores": {
+		"name": "Spores",
+		"short_name": "",
+		"icon": preload("res://Sprites/Loot/spores.png"),
+	},
+	"Storm Feather": {
+		"name": "Storm Feather",
+		"short_name": "",
+		"icon": preload("res://Sprites/Loot/storm_feather.png"),
+	},
+	"Widow Silk": {
+		"name": "Widow Silk",
+		"short_name": "",
+		"icon": preload("res://Sprites/Loot/widow_silk.png"),
+	},
+	"Wisp Dust": {
+		"name": "Wisp Dust",
+		"short_name": "",
+		"icon": preload("res://Sprites/Loot/wisp_dust.png"),
+	},
 }
