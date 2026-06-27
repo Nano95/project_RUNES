f12a611d15e1e73ee4afea2bbd8e27e289c21bb3
Alec <alec.burmeister@gmail.com>
2026-06-27 12:25:34 -0500

    UI Setup

--- Scripts/FocusChamber.gd
+++ Scripts/FocusChamber.gd
@@ -2,6 +2,10 @@ extends Control
 class_name FocusChamber
 
 @export var number_pad:Control
+@export var code_label:Label
+@export var player_label:Label 
+@export var timer_progress_bar:TextureProgressBar
+@export var player_input:String = "" # What the player types
 
 func connect_number_pad() -> void:
 	# Loop through all Button children of this Control node
