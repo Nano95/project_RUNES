7d4c8861a74d61abf9d6556c1117ef3a8c2e771b
Alec <alec.burmeister@gmail.com>
2026-07-05 10:22:29 -0500

    time and timer/cooldown in Focus Chamber working

--- Scripts/FocusChamberModal.gd
+++ Scripts/FocusChamberModal.gd
@@ -17,6 +17,7 @@ class_name FocusChamberModal
 @export var trialView:Control
 @export var goToTrialBtn:Button
 @export var backBtn2:Button
+@export var trial_timer:Timer
 var main:MainNode
 
 func _ready() -> void:
@@ -24,6 +25,19 @@ func _ready() -> void:
 	connect_buttons()
 	setup_labels()
 	Utils.animate_summary_in_happy(self)
+	
+	if (is_focus_chamber_available()):
+		goToTrialBtn.text = "Go!"
+		trial_timer.stop()
+	else:
+		goToTrialBtn.disabled = true
+		var remaining := get_focus_chamber_remaining()
+		goToTrialBtn.text = format_hms(remaining)
+
+		# Start ticking every second
+		trial_timer.wait_time = 1.0
+		trial_timer.start()
+		trial_timer.timeout.connect(_on_trial_timer_tick)
 
 func setup(m:MainNode) -> void:
 	main = m
@@ -55,9 +69,43 @@ func display_main_view(isVisible:bool=true) -> void:
 
 func spawn_focus_chamber(is_practice:bool=false, mode:String="") -> void:
 	main.spawn_focus_chamber(is_practice, mode)
+	
+	if (is_practice):
+		return
+	
+	@warning_ignore("narrowing_conversion")
+	var now: int = Time.get_unix_time_from_system()
+	#var cooldown_seconds: int = 60 # while testing, only a minute
+	var cooldown_seconds: int = 3600
+	# an hour from now is what is being set here
+	main.game_data.focus_chamber_time_available = int(now + cooldown_seconds)
+
+func is_focus_chamber_available() -> bool:
+	var now := Time.get_unix_time_from_system()
+	return now >= main.game_data.focus_chamber_time_available
+
+func get_focus_chamber_remaining() -> int:
+	var now := Time.get_unix_time_from_system()
+	return max(0, main.game_data.focus_chamber_time_available - now)
 
 func setup_labels() -> void:
 	easy_hs.text = "Best: " + str(main.game_data.focus_chamber_practice_easy_highscore)
 	med_hs.text = "Best: " + str(main.game_data.focus_chamber_practice_med_highscore)
 	hard_hs.text = "Best: " + str(main.game_data.focus_chamber_practice_hard_highscore)
 	trial_hs.text = "Best: " + str(main.game_data.focus_chamber_trial_highscore)
+
+func format_hms(seconds: int) -> String:
+	@warning_ignore("integer_division")
+	var mins = (seconds % 3600) / 60
+	var secs = seconds % 60
+	return "%02d:%02d" % [mins, secs]
+
+func _on_trial_timer_tick() -> void:
+	var remaining := get_focus_chamber_remaining()
+
+	if (remaining <= 0):
+		trial_timer.stop()
+		goToTrialBtn.text = "Go!"
+		goToTrialBtn.disabled = false
+	else:
+		goToTrialBtn.text = format_hms(remaining)
